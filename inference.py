f16ae30b2a5a785c2916f3e5ac9cdf1cea593d65
tom99763 <69715105+tom99763@users.noreply.github.com>
2021-05-16 13:04:56 +0800

    Update inference.py

--- inference.py
+++ inference.py
@@ -3,7 +3,7 @@ import cv2
 
 
 
-def draw_faces(img, bboxes, landmarks, scores):
+def draw_faces(img, bboxes, landmarks):
     for box in bboxes:
         img = cv2.rectangle(img, (int(box[0]), int(box[1])),
                             (int(box[2]), int(box[3])), (255, 0, 0), 2)
@@ -17,5 +17,5 @@ mtcnn=MTCNN(pnet_path=pnet_weights_path)
 img=cv2.imread('./test_imgs/test.jpg')
 boxes=mtcnn(img)
 
-img = draw_faces(img, boxes, None, None)
+img = draw_faces(img, boxes, None)
 cv2.imwrite('./test_imgs/result_rnet.jpg', img)
