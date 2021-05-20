bea3e51bd3e61a45c0c14c5ac781aef026e3bfd7
tom99763 <69715105+tom99763@users.noreply.github.com>
2021-05-20 13:42:18 +0800

    Update inference.py

--- inference.py
+++ inference.py
@@ -2,8 +2,6 @@ from mtcnn import MTCNN
 import cv2
 
 
-
-
 def draw_faces(img, bboxes, landmarks, scores):
     if landmarks==None:
         for box in bboxes:
@@ -23,23 +21,61 @@ def draw_faces(img, bboxes, landmarks, scores):
     return img
 
 
+def video_show(video=None, output=None,mtcnn=None):
+    if video is None:
+        vid = cv2.VideoCapture(0)
+    else:
+        vid = cv2.VideoCapture(video)
+
+    out = None
+    if output is not None:
+        fps = vid.get(cv2.CAP_PROP_FPS)
+        _, img = vid.read()
+        h, w, _ = img.shape
+        fourcc = cv2.VideoWriter_fourcc(*'XVID')
+        out = cv2.VideoWriter(output, fourcc, fps, (w, h))
+
+    frames = 0
+    i = 0
+    while True:
+        _, img = vid.read()
+        if img is None:
+            break
+        bboxes, landmarks, scores = mtcnn(img)
+        frames += 1
+        img = draw_faces(img, bboxes, landmarks, scores)
+
+        #cv2.imshow('demo', img)
+        if out is not None:
+            out.write(img)
+        if cv2.waitKey(1) & 0xFF in [27, ord('q')]:
+            break
+        i += 1
+    cv2.destroyAllWindows()
+
+
+
 pnet_weights_path='./train/Weights/pnet_weights_file/pnet_weights'
 rnet_weights_path='./train/Weights/rnet_weights_file/rnet_weights'
 onet_weights_path='./train/Weights/onet_weights_file/onet_weights'
-
 mtcnn=MTCNN(pnet_path=pnet_weights_path,rnet_path=rnet_weights_path,onet_path=onet_weights_path)
 
-name = 'test2'
+video_path='./test/warriors_vs_memphis.mp4'
+video_show(video_path,'test/warriors_vs_memphis_detect.mp4',mtcnn)
 
+
+
+
+'''
+name = 'splash_brothers'
 img=cv2.imread(f'./test/{name}.jpg')
-
+'''
 
 '''
 boxes=mtcnn.p_step(img)
 img_res = draw_faces(img, boxes, None, None)
 cv2.imwrite(f'./test/{name}_result_pnet.jpg', img_res)
 '''
-
 
 '''
 boxes=mtcnn.p_step(img)
@@ -48,8 +84,9 @@ img_res = draw_faces(img, boxes, None, None)
 cv2.imwrite(f'./test/{name}_result_rnet.jpg', img_res)
 '''
 
-
+'''
 boxes,landmarks,scores=mtcnn(img)
 img_res = draw_faces(img, boxes, landmarks, scores)
 cv2.imwrite(f'./test/{name}_result_output.jpg', img_res)
+'''
 
